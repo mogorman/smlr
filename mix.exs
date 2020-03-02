815582a28cd6010c682a746a1b34e9d6e46dfdde
Matthew O'Gorman <mog@rldn.net>
2020-03-02 18:44:03 -0500

    adding deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,27 @@ defmodule Smlr.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/mogorman/smlr",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+      ],
+      # Docs
+      name: "Smlr",
+      source_url: "https://github.com/mogorman/smlr",
+      homepage_url: "https://github.com/mogorman/smlr",
+      docs: [
+        # The main page in the docs
+        main: "Smlr",
+        logo: "smlr.png",
+        extras: ["README.md"],
+      ],
     ]
   end
 
@@ -18,11 +38,28 @@ defmodule Smlr.MixProject do
     ]
   end
 
+  defp description() do
+    "Smlr provides a plug for compressing dynamic content in phoenix and the ability to cache it temporarily."
+  end
+
+  defp package() do
+    [
+      maintainers: ["Matthew O'Gorman mog@rldn.net"],
+      links: %{"GitHub" => "https://github.com/mogorman/smlr"},
+      licenses: ["MIT"],
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: [:dev, :test]},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:mock, "~> 0.3.0", only: :test},
+      {:plug, ">= 1.6.0"},
+      {:cachex, "~> 3.2"}
     ]
   end
 end
