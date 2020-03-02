1c8ec310d65df12980eb31e53aff25460e1f8126
Matthew O'Gorman <mog@rldn.net>
2020-03-02 18:33:27 -0500

    an initial commit

---
+++ lib/smlr.ex
@@ -0,0 +1,18 @@
+defmodule Smlr do
+  @moduledoc """
+  Documentation for Smlr.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Smlr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
